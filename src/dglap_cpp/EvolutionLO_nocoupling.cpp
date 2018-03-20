2f6c8e9831c9c05f2843680a6ffd4ce3787e6e1c
Heikki Mäntysaari <heikki.mantysaari@jyu.fi>
2018-03-20 17:34:45 +0200

    Mellin transform for singlet IC analytically

--- src/dglap_cpp/EvolutionLO_nocoupling.cpp
+++ src/dglap_cpp/EvolutionLO_nocoupling.cpp
@@ -122,7 +122,14 @@ void EvolutionLO_gluon::reno(complex<double> *fn, double alpq, int nmax, int cou
             + 1.0 / (xn - lambdag - 1.0)
                                     );
         
-       
+         
+        // ic with (1-x)^2
+        /*
+        complex<double> gln = ag * 2.0 / ( (-1.0 + xn - lambdag)*(xn - lambdag) * (1.0 + xn - lambdag) );
+       */
+        // (1-x)^1
+        //complex<double> gln = ag * ( 1.0 / ( (-1.0 + xn - lambdag) * (xn - lambdag) ) );
+        
         /*
         complex<double> gln = ag * (
                                     1.0 / (xn + 9.0 - lambdag)
